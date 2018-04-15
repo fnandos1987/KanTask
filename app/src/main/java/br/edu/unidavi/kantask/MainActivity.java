6d2e1bbcc0bf8c4de393cfe6a7f409fde71efc86
fernando.schwambach <fnandos1987@gmail.com>
2018-04-15 18:03:19 -0300

    implementado TabLayout

--- app/src/main/java/br/edu/unidavi/kantask/MainActivity.java
+++ app/src/main/java/br/edu/unidavi/kantask/MainActivity.java
@@ -1,7 +1,14 @@
 package br.edu.unidavi.kantask;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.edu.unidavi.kantask.adapter.TabsAdapter;
+import br.edu.unidavi.kantask.fragment.DoFragment;
+import br.edu.unidavi.kantask.fragment.DoingFragment;
+import br.edu.unidavi.kantask.fragment.DoneFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,14 +19,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabsAdapter adapter = new AbasAdapter(getSupportFragmentManager());
-        adapter.adicionar( new PrimeiroFragment() , "Primeira Aba");
-        adapter.adicionar( new SegundoFragment(), "Segunda Aba");
+        TabsAdapter adapter = new TabsAdapter(getSupportFragmentManager());
+        adapter.addTab( new DoFragment() , "A Fazer");
+        adapter.addTab( new DoingFragment(), "Fazendo");
+        adapter.addTab( new DoneFragment(), "Feito");
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.abas_view_pager);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.tabs_view_pager);
         viewPager.setAdapter(adapter);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.abas);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
     }
 }
